29a5f0d833c3110f14f58e56b9bbdac152d2682f
CawsualCrow <25thunderbirdiv@gmail.com>
2025-03-14 12:54:24 -0400

    fixed

--- backend/server/routes/getStationsById.js
+++ backend/server/routes/getStationsById.js
@@ -1,4 +1,6 @@
 const Stop = require("../models/stationModel"); // Ensure correct model path
+const express = require("express");
+const router = express.Router();
 
 const MBTA_API_BASE = "https://api-v3.mbta.com";
 const MBTA_API_KEY = process.env.MBTA_API_KEY; // Store your MBTA API key in .env
